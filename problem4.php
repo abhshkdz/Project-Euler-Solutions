daddfb537de9b46b59ed598f65e01307437fc60f
Abhishek Das <das.abhshk@gmail.com>
2012-02-07 23:46:33 +0530

    problem-4

---
+++ problem4.php
@@ -0,0 +1,29 @@
+<?php
+
+function palindrome ($a)
+{
+	$b=$a;
+	$c=0;	
+	while ($b)
+	{
+		$c+=$b%10;
+		$c*=10;
+		$b/=10;
+		$b=(int)$b;
+	}
+	//echo $c;
+	$c/=10;
+	//echo "$c<br>";
+	if ($a==$c) return 1;
+	return 0;
+}
+	
+$n=0;
+	
+for ($i=100; $i<1000; $i++)	
+	for ($j=100; $j<1000; $j++)
+		if(palindrome($i*$j) && $n<($i*$j)) $n=($i*$j);	
+
+echo $n;
+
+?>
