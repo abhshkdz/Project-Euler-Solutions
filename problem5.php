532a216b807d8e43eb01823ef2daef2a82a3acee
Abhishek Das <das.abhshk@gmail.com>
2012-02-08 07:52:33 +0530

    problem-5 & readme

---
+++ problem5.php
@@ -0,0 +1,22 @@
+<?php
+
+$yes=FALSE;
+for ($i=20; !$yes; $i+=20) 
+{
+  for ($j=2; $j<20; $j++) 
+  {
+    if ($i%$j==0) 
+      $yes = TRUE;
+    else {
+      $yes = FALSE;
+      break;
+    }
+  }
+  if ($yes) 
+  {
+    echo $i;
+    break;
+  }
+}
+
+?>
